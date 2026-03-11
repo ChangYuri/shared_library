d2f0251af5eae520e95f33ef56e5ad31adf4a681
Yuri Chang <yurichang@uchicago.edu>
2026-03-11 16:00:40 -0500

    dlystg

--- vars/deployStage.groovy
+++ vars/deployStage.groovy
@@ -3,5 +3,21 @@ def call(String environment, String imageTag) {
         input message: "do you want to deploy ${imageTag} to production?", ok: "yes"
     }
     echo "Deploying ${imageTag} to ${environment}"
-    // will add kubernetes deployment commands here
+
+
+    def services = ['frontend', 'order-service', 'product-service']
+    
+    for (service in services) {
+        sh """
+            kubectl set image deployment/${service} \
+            ${service}=yurichang/${service}:${imageTag} \
+            -n ${environment}
+        """
+    }
+    
+    for (service in services) {
+        sh "kubectl rollout status deployment/${service} -n ${environment}"
+    }
+
+    echo "Deploy successful ${imageTag} to ${environment} environment"
 }
