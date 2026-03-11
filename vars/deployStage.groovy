3636ccf3411904401ddf21bc351de2b1da9b8be9
Yuri Chang <yurichang@uchicago.edu>
2026-03-11 16:23:09 -0500

    deploy stage

--- vars/deployStage.groovy
+++ vars/deployStage.groovy
@@ -1,13 +1,5 @@
-def call(String environment, String imageTag) {
+def call(String environment) {
     if (environment == 'prod') {
-        input message: "do you want to deploy ${imageTag} to production?", ok: "yes"
+        input message: "are you sure you want to deploy to production?", ok: "Deploy"
     }
-    echo "Deploying ${imageTag} to ${environment}"
-
-
-    sh "kubectl set image deployment/${serviceName} ${serviceName}=yurichang/${serviceName}:${imageTag} -n ${environment}"
-    sh "kubectl rollout status deployment/${serviceName} -n ${environment}"
-
-
-    echo "Deploy successful ${imageTag} to ${environment} environment"
 }
